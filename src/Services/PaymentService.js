7e26faf415f8ce6133b8fb84fbc15a7dfe858a3e
unknown <jamesgalima12321@gmail.com>
2022-11-28 11:33:51 +0800

    grabpay

--- src/Services/PaymentService.js
+++ src/Services/PaymentService.js
@@ -81,15 +81,21 @@ class PaymentService {
         let body = req.body;
         let params = req.params;
         try {
+
             let contract_uuid = params.contract_uuid;
             let type = params.type;
             let contract = await CONTRACT.get( {uuid:contract_uuid,status:'inprogress'} );
-            let user = await USER.get( {id:contract.client_id} );
-            let user_profile = await USER_PROFILE.get( {user_id:contract.client_id} );
+            console.log(contract_uuid);
             if(!contract){
                 return {status:404,message:'Contract does not exist'};
             }
+            let user = await USER.get( {id:contract.client_id} );
+            let user_profile = await USER_PROFILE.get( {user_id:contract.client_id} );
+            console.log('falala');
+
+            
             let proposal = await PROPOSAL.get( {uuid:contract.proposal_uuid} );
+
             if(type === 'gcash' || type === 'grab_pay'){
                 let fields = {data:{attributes:{amount:parseInt(proposal.fee + "00"),billing:{email:user.email,name: user_profile.first_name + " " + user_profile.last_name},redirect:{success:"https://developers.paymongo.com/docs/accepting-gcash-payments",failed:"https://www.facebook.com/watch/?v=515061943985743"},type:type,currency:"PHP"}}};
                 const headers = {
@@ -117,7 +123,6 @@ class PaymentService {
                     return {status:500,message:response};  
                 }
                 let checkout_url = response.data.attributes.redirect.checkout_url;
-
                 return {status:200,message:'success',data:checkout_url};
             }
             return {status:404,message:'E wallet does not exist'};
